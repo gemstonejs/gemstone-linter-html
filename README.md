f768c86344e57411ea957353a2170416c48c710f
Ralf S. Engelschall <rse@engelschall.com>
2017-04-05 11:23:13 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gemstone-linter-html
-Linter for HTML
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Linter HTML
+====================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-linter-html.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-linter-html.png" alt=""/>
+
+About
+-----
+
+This is the Linter for HTML files in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+It is based on [HTMLHint](http://htmlhint.com/).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-linter-html.js
@@ -0,0 +1,56 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  load external requirements  */
+const path             = require("path")
+const fs               = require("fs-promise")
+const HTMLHint         = require("htmlhint").HTMLHint
+const HTMLHintGemstone = require("gemstone-config-htmlhint")
+
+/*  add custom Gemstone rules  */
+let config = HTMLHintGemstone(HTMLHint)
+
+/*  exported API function  */
+module.exports = async function (filenames, opts = {}, report = { sources: {}, findings: [] }) {
+    /*  interate over all source files  */
+    let passed = true
+    if (typeof opts.progress === "function")
+        opts.progress(0.0, "linting HTML: starting")
+    for (let i = 0; i < filenames.length; i++) {
+        /*  indicate progress  */
+        if (typeof opts.progress === "function")
+            opts.progress(i / filenames.length, `linting HTML: ${filenames[i]}`)
+
+        /*  read source code  */
+        let source = await fs.readFile(filenames[i], "utf8")
+
+        /*  determine name  */
+        let name = path.relative(process.cwd(), filenames[i])
+
+        /*  lint the source code  */
+        let rules = Object.assign({}, config, opts.rules)
+        let messages = HTMLHint.verify(source, rules)
+        if (messages.length > 0) {
+            for (let j = 0; j < messages.length; j++) {
+                report.findings.push({
+                    ctx:      "HTML",
+                    filename: name,
+                    line:     messages[j].line,
+                    column:   messages[j].col,
+                    message:  messages[j].message,
+                    ruleProc: "htmlhint",
+                    ruleId:   messages[j].rule.id
+                })
+            }
+            report.sources[name] = source
+            passed = false
+        }
+    }
+    if (typeof opts.progress === "function")
+        opts.progress(1.0, "")
+    return passed
+}
+
